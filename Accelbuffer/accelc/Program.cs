3eeefd9faa96e0c191c661e525a2fcdf9743eff2
雨涵 金 <q1024751595@163.com>
2019-12-21 19:43:02 +0800

    move files

---
+++ Accelbuffer/accelc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace AccelbufferCompiler
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: accelc [input files]\n");
+                return;
+            }
+
+            Console.WriteLine("not supported, in progress...");
+            return;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string sourceFilePath = args[i];
+
+                if (!File.Exists(sourceFilePath))
+                {
+                    Console.WriteLine($"file: '{sourceFilePath}' not found!\n");
+                    continue;
+                }
+
+                string outputFilePath = Path.ChangeExtension(sourceFilePath, null) + "SerializeProxy.cs";
+
+                Console.WriteLine($"Generate at '{outputFilePath}'!");
+            }
+        }
+    }
+}
